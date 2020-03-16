4d00e18d35f2ea0d10f25199ffbfb11ee95625f2
vadymshukr <vadymshukr@gmail.com>
2020-03-16 21:01:58 +0200

    Updates

---
+++ precache-manifest.559d600c0a538340ed5b7eec99b54921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8a537bee99cd53d96823e2c6d732e0",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "833cb0d4c1dd08b5a5c4",
+    "url": "/drum-machine/static/css/main.50637465.chunk.css"
+  },
+  {
+    "revision": "d8abd6cef6c86e953e53",
+    "url": "/drum-machine/static/js/2.f7ea7386.chunk.js"
+  },
+  {
+    "revision": "35e9035c4541049d41232fe0bf96f70b",
+    "url": "/drum-machine/static/js/2.f7ea7386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "833cb0d4c1dd08b5a5c4",
+    "url": "/drum-machine/static/js/main.8628c6b6.chunk.js"
+  },
+  {
+    "revision": "8693eb012ac077547e9e",
+    "url": "/drum-machine/static/js/runtime-main.1b00ec3c.js"
+  }
+]);
